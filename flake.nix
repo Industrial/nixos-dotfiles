ab32e689ad7de0b4fecd1a32f72e86f77105739d
Tom Wieland <tom.wieland@gmail.com>
2023-04-30 22:49:40 +0200

    refactor: everything in features!

--- flake.nix
+++ flake.nix
@@ -22,25 +22,23 @@
         inherit system;
 
         modules = [
-          #./features/stylix
-          #inputs.stylix.nixosModules.stylix
-          ./features/boot
-          ./features/console
-          ./features/disks
-          ./features/docker
-          ./features/fonts
-          ./features/graphics
-          ./features/i18n
-          ./features/lutris
-          ./features/networking
-          ./features/nix
-          ./features/printing
-          ./features/shell
-          ./features/sound
-          ./features/time
-          ./features/users
-          ./features/window-manager
-          ./features/xfce
+          ./features/system/boot
+          ./features/system/console
+          ./features/system/disks
+          ./features/system/docker
+          ./features/system/fonts
+          ./features/system/graphics
+          ./features/system/i18n
+          ./features/system/lutris
+          ./features/system/networking
+          ./features/system/nix
+          ./features/system/printing
+          ./features/system/shell
+          ./features/system/sound
+          ./features/system/time
+          ./features/system/users
+          ./features/system/window-manager
+          ./features/system/xfce
           ({...}: {
             imports = [
               ./hardware-configuration.nix
@@ -65,7 +63,86 @@
       "tom@drakkar" = inputs.home-manager.lib.homeManagerConfiguration {
         pkgs = pkgs;
         modules = [
-          ./users/tom/home
+          inputs.stylix.homeManagerModules.stylix
+          #./features/home/gnome
+          #./features/home/zsh
+          ./features/home/base16-shell
+          ./features/home/fish
+          ./features/home/git
+          ./features/home/lutris
+          ./features/home/matrix
+          ./features/home/mpv
+          ./features/home/neovim
+          ./features/home/stylix
+          ./features/home/taskwarrior
+          ./features/home/tmux
+          ./features/home/vscode
+          ./features/home/xfce
+          ({...}: {
+            home = {
+              username = "tom";
+              homeDirectory = "/home/tom";
+              stateVersion = "20.09";
+
+              sessionVariables = {
+                EDITOR = "nvim";
+                GIT_EDITOR = "nvim";
+                VISUAL = "nvim";
+                PAGER = "nvim";
+                DIFFPROG = "nvim -d";
+                MANPAGER = "nvim +Man!";
+                MANWIDTH = 999;
+              };
+
+              packages = with pkgs; [
+                usbutils
+                android-tools
+                appimage-run
+                bitwarden
+                bookworm
+                chromium
+                direnv
+                discord
+                docker-compose
+                exa
+                fd
+                filezilla
+                firefox
+                fzf
+                gcc
+                gitkraken
+                gnomeExtensions.material-shell
+                htop
+                #libreoffice
+                meld
+                nethogs
+                ripgrep
+                slack
+                spotify
+                starship
+                steam
+                transmission-gtk
+                vit
+                vlc
+                xclip
+                xsel
+                yubikey-personalization-gui
+                zeal
+
+                # Tor
+                tor-browser-bundle-bin
+
+                # Python
+                stdenv.cc.cc.lib
+                python3
+                virtualenv
+                poetry
+
+                # Java
+                jre8
+              ];
+            };
+          })
         ];
       };
     };
