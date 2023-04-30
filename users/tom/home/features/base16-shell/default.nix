90b794ee02f5ecaa742e68ba1748b35ee1fef831
Tom Wieland <tom.wieland@gmail.com>
2023-04-30 15:20:06 +0200

    refactor(base16-shell): add base16-builder

--- users/tom/home/features/base16-shell/default.nix
+++ users/tom/home/features/base16-shell/default.nix
@@ -9,7 +9,7 @@ let
       rev = "d0737249d4c8bb26dc047ea9fba0054ae7024c04";
       sha256 = "sha256-X6Pcu/LM9PSaUwLxHoklXNkSEz+X1+cIt8lmu6tViMk=";
     };
-    # buildInputs = [ pkgs.base16-builder ];
+    buildInputs = [ pkgs.base16-builder ];
     installPhase = ''
       mkdir -p $out/share/base16/shell
       cp -r scripts $out/share/base16/shell
