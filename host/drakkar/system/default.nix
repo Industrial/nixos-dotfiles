e821f252044c60da1152eec0628d08b727bdb5db
Tom Wieland <tom.wieland@gmail.com>
2023-11-17 11:54:12 +0100

    feat(system): merge everything into feature category directories

--- host/drakkar/system/default.nix
+++ host/drakkar/system/default.nix
@@ -2,30 +2,43 @@
   imports = [
     ./graphics
     ./hardware-configuration.nix
-    ../../../features/system/bluetooth
-    ../../../features/system/boot
-    ../../../features/system/chromium
-    ../../../features/system/console
-    ../../../features/system/docker
-    ../../../features/system/fish
-    ../../../features/system/fonts
-    ../../../features/system/git
-    ../../../features/system/home-manager
-    ../../../features/system/i18n
-    ../../../features/system/lutris
-    ../../../features/system/networking
-    ../../../features/system/nix
-    ../../../features/system/p7zip
-    ../../../features/system/printing
-    ../../../features/system/shell
-    ../../../features/system/sound
-    ../../../features/system/starship
-    ../../../features/system/syncthing
-    ../../../features/system/system
-    ../../../features/system/time
-    ../../../features/system/unrar
-    ../../../features/system/users
-    ../../../features/system/window-manager
-    ../../../features/system/xfce
+
+    # CLI
+    ../../../features/system/cli/fish
+    ../../../features/system/cli/p7zip
+    ../../../features/system/cli/starship
+    ../../../features/system/cli/unrar
+
+    # Network
+    ../../../features/system/network/syncthing
+    # ../../../features/system/network/tor
+
+    # Operating System
+    ../../../features/system/operating-system/bluetooth
+    ../../../features/system/operating-system/boot
+    ../../../features/system/operating-system/console
+    ../../../features/system/operating-system/fonts
+    ../../../features/system/operating-system/home-manager
+    ../../../features/system/operating-system/i18n
+    ../../../features/system/operating-system/lutris
+    ../../../features/system/operating-system/networking
+    ../../../features/system/operating-system/nix
+    ../../../features/system/operating-system/printing
+    ../../../features/system/operating-system/shell
+    ../../../features/system/operating-system/sound
+    ../../../features/system/operating-system/system
+    ../../../features/system/operating-system/time
+    ../../../features/system/operating-system/users
+    ../../../features/system/operating-system/window-manager
+
+    # Programming
+    ../../../features/system/programming/docker
+    ../../../features/system/programming/git
+    # ../../../features/system/programming/haskell.nix
+
+    # Window Manager
+    ../../../features/system/window-manager/chromium
+    # ../../../features/system/window-manager/hyprland
+    ../../../features/system/window-manager/xfce
   ];
 }
