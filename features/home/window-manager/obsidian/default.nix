93e479153c3f56af251fc094157602d8b655f36d
Tom Wieland <tom.wieland@gmail.com>
2023-12-16 16:10:19 +0100

    Feature/obsidian fix (#7)
    * fix: obsidian requires an insecure package. Fix this ASAP
    
    * fix: use pkgs.lib.optional

--- features/home/window-manager/obsidian/default.nix
+++ features/home/window-manager/obsidian/default.nix
@@ -1,6 +1,15 @@
-# Not Taker.
-{pkgs, ...}: {
+# Note Taker.
+{
+  c9config,
+  pkgs,
+  ...
+}: {
   home.packages = with pkgs; [
     obsidian
   ];
+
+  # TODO: Fix this as soon as Obsidian makes a new release.
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages =
+    pkgs.lib.optional (pkgs.obsidian.version == "1.4.16") "electron-25.9.0";
 }
