1e2ac4fe0b48900ad807e07ed53d37979c03d8ce
Tom Wieland <tom.wieland@gmail.com>
2023-12-16 15:54:19 +0100

    fix: obsidian requires an insecure package. Fix this ASAP

--- features/home/window-manager/obsidian/default.nix
+++ features/home/window-manager/obsidian/default.nix
@@ -1,6 +1,15 @@
-# Not Taker.
-{pkgs, ...}: {
+# Note Taker.
+{
+  c9config,
+  pkgs,
+  ...
+}: {
   home.packages = with pkgs; [
     obsidian
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-25.9.0"
   ];
 }
