96c68e9214ca7a34ad8a55e0b5fb91df44fb6a2f
Tom Wieland <tom.wieland@gmail.com>
2024-01-05 21:09:51 +0100

    feat(obsidian): version update

--- features/home/window-manager/obsidian/default.nix
+++ features/home/window-manager/obsidian/default.nix
@@ -11,5 +11,5 @@
   # TODO: Fix this as soon as Obsidian makes a new release.
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.permittedInsecurePackages =
-    pkgs.lib.optional (pkgs.obsidian.version == "1.4.16") "electron-25.9.0";
+    pkgs.lib.optional (pkgs.obsidian.version == "1.5.3") "electron-25.9.0";
 }
