b334755c25ca737a31f8873d226ff698d14b0b94
Tom Wieland <tom.wieland@gmail.com>
2024-03-17 10:03:44 +0100

    refactor: put env vars in fish shell

--- features/system/cli/fish/default.nix
+++ features/system/cli/fish/default.nix
@@ -29,6 +29,15 @@ in {
     alias z='zellij --session system'
     alias za='zellij attach system'
 
+    # Variables
+    set -x EDITOR "nvim"
+    set -x GIT_EDITOR "nvim"
+    set -x DIFFPROG "nvim -d"
+    set -x XDG_CACHE_HOME "$HOME/.cache"
+    set -x XDG_CONFIG_HOME "$HOME/.config"
+    set -x XDG_DATA_HOME "$HOME/.local/share"
+    set -x XDG_STATE_HOME "$HOME/.local/state"
+
     # PATH
     fish_add_path $HOME/.bin
 
