38c72dfe0fe370ebf8927676eb0b5b28efd863c1
Tom Wieland <tom.wieland@gmail.com>
2024-04-08 17:56:37 +0200

    Add Tor client and enable Tor service in NixOS configuration

---
+++ features/network/tor/default.nix
@@ -0,0 +1,11 @@
+{
+  settings,
+  pkgs,
+  ...
+}: {
+  # services.tor.client.port = 9051;
+  # services.tor.client.socksListenAddress = "127.0.0.1";
+  services.tor.client.enable = true;
+  services.tor.enable = true;
+  services.tor.relay.enable = false;
+}
