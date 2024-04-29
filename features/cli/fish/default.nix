8e2ddf9c3b451ee96999e4f546bb5eb3772160b3
Tom Wieland <tom.wieland@gmail.com>
2024-04-29 20:20:49 +0200

    feat(osx): remove some osx settings

--- features/cli/fish/default.nix
+++ features/cli/fish/default.nix
@@ -118,7 +118,6 @@ in {
     defaults write -g InitialKeyRepeat -int 15
     defaults write -g AppleShowAllFiles -bool true
     defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
-    defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
-    defaults write -g com.apple.gamed Disabled -bool true
+    defaults write -g com.apple.gamed Disabled
   '';
 }
