2b19b1c2d03130cf43eaff4bf03d99ff89e2fde6
Tom Wieland <tom.wieland@gmail.com>
2024-05-02 18:57:13 +0200

    fix(vscode): pin copilot-chat version for compatibility

--- features/programming/vscode/default.nix
+++ features/programming/vscode/default.nix
@@ -7,78 +7,87 @@
   extensions = inputs.nix-vscode-extensions.extensions.${pkgs.system};
 
   vscodeWithExtensions = pkgs.vscode-with-extensions.override {
-    vscodeExtensions = [
-      # Themes
-      extensions.vscode-marketplace.zhuangtongfa.material-theme
-      extensions.vscode-marketplace.pkief.material-icon-theme
-      extensions.vscode-marketplace.tintedtheming.base16-tinted-themes
+    vscodeExtensions =
+      [
+        # Themes
+        extensions.vscode-marketplace.zhuangtongfa.material-theme
+        extensions.vscode-marketplace.pkief.material-icon-theme
+        extensions.vscode-marketplace.tintedtheming.base16-tinted-themes
 
-      # Vim
-      extensions.vscode-marketplace.vscodevim.vim
+        # Vim
+        extensions.vscode-marketplace.vscodevim.vim
 
-      # Visual Feedback
-      extensions.vscode-marketplace.usernamehw.errorlens
-      extensions.vscode-marketplace.vspacecode.whichkey
+        # Visual Feedback
+        extensions.vscode-marketplace.usernamehw.errorlens
+        extensions.vscode-marketplace.vspacecode.whichkey
 
-      # Completion
-      extensions.vscode-marketplace.github.copilot
-      extensions.vscode-marketplace-release.github.copilot-chat
+        # Completion
+        extensions.vscode-marketplace.github.copilot
+        # extensions.vscode-marketplace-release.github.copilot-chat
 
-      # File Types
-      ## GraphQL
-      extensions.vscode-marketplace.graphql.vscode-graphql-syntax
-      extensions.vscode-marketplace.graphql.vscode-graphql
-      ## Markdown
-      extensions.vscode-marketplace.yzhang.markdown-all-in-one
-      ## JavaScript / TypeScript
-      extensions.vscode-marketplace.dbaeumer.vscode-eslint
-      extensions.vscode-marketplace.denoland.vscode-deno
-      extensions.vscode-marketplace.ms-vscode.js-debug
-      extensions.vscode-marketplace.ms-vscode.js-debug-companion
-      ## Python
-      extensions.vscode-marketplace.littlefoxteam.vscode-python-test-adapter
-      extensions.vscode-marketplace.ms-python.black-formatter
-      extensions.vscode-marketplace.ms-python.debugpy
-      extensions.vscode-marketplace.ms-python.flake8
-      extensions.vscode-marketplace.ms-python.isort
-      extensions.vscode-marketplace.ms-python.python
-      extensions.vscode-marketplace.ms-python.vscode-pylance
-      extensions.vscode-marketplace.tamasfe.even-better-toml
-      ## Jupyter
-      extensions.vscode-marketplace.ms-toolsai.jupyter
-      extensions.vscode-marketplace.ms-toolsai.jupyter-renderers
-      ## Nix
-      extensions.vscode-marketplace.bbenoist.nix
-      extensions.vscode-marketplace.jnoortheen.nix-ide
-      extensions.vscode-marketplace.kamadorueda.alejandra
-      ## YAML
-      extensions.vscode-marketplace.redhat.vscode-yaml
-      ## Docker
-      extensions.vscode-marketplace.ms-azuretools.vscode-docker
-      ## Dotenv
-      extensions.vscode-marketplace.mikestead.dotenv
-      ## Git
-      extensions.vscode-marketplace.mhutchie.git-graph
-      extensions.vscode-marketplace.sugatoray.vscode-git-extension-pack
-      ## PlantUML
-      extensions.vscode-marketplace.jebbs.plantuml
-      ## AutoHotkey
-      # extensions.vscode-marketplace.mark-wiemer.vscode-autohotkey-plus-plus
-      extensions.vscode-marketplace.thqby.vscode-autohotkey2-lsp
+        # File Types
+        ## GraphQL
+        extensions.vscode-marketplace.graphql.vscode-graphql-syntax
+        extensions.vscode-marketplace.graphql.vscode-graphql
+        ## Markdown
+        extensions.vscode-marketplace.yzhang.markdown-all-in-one
+        ## JavaScript / TypeScript
+        extensions.vscode-marketplace.dbaeumer.vscode-eslint
+        extensions.vscode-marketplace.denoland.vscode-deno
+        extensions.vscode-marketplace.ms-vscode.js-debug
+        extensions.vscode-marketplace.ms-vscode.js-debug-companion
+        # ## Python
+        # extensions.vscode-marketplace.littlefoxteam.vscode-python-test-adapter
+        # extensions.vscode-marketplace.ms-python.black-formatter
+        # extensions.vscode-marketplace.ms-python.debugpy
+        # extensions.vscode-marketplace.ms-python.flake8
+        # extensions.vscode-marketplace.ms-python.isort
+        # extensions.vscode-marketplace.ms-python.python
+        # extensions.vscode-marketplace.ms-python.vscode-pylance
+        # extensions.vscode-marketplace.tamasfe.even-better-toml
+        # ## Jupyter
+        # extensions.vscode-marketplace.ms-toolsai.jupyter
+        # extensions.vscode-marketplace.ms-toolsai.jupyter-renderers
+        ## Nix
+        extensions.vscode-marketplace.bbenoist.nix
+        extensions.vscode-marketplace.jnoortheen.nix-ide
+        extensions.vscode-marketplace.kamadorueda.alejandra
+        ## YAML
+        extensions.vscode-marketplace.redhat.vscode-yaml
+        # ## Docker
+        # extensions.vscode-marketplace.ms-azuretools.vscode-docker
+        ## Dotenv
+        extensions.vscode-marketplace.mikestead.dotenv
+        ## Git
+        extensions.vscode-marketplace.mhutchie.git-graph
+        extensions.vscode-marketplace.sugatoray.vscode-git-extension-pack
+        # ## PlantUML
+        # extensions.vscode-marketplace.jebbs.plantuml
+        # ## AutoHotkey
+        # # extensions.vscode-marketplace.mark-wiemer.vscode-autohotkey-plus-plus
+        # extensions.vscode-marketplace.thqby.vscode-autohotkey2-lsp
 
-      # Testing
-      extensions.vscode-marketplace.hbenl.vscode-test-explorer
-      extensions.vscode-marketplace.ms-playwright.playwright
-      extensions.vscode-marketplace.ms-vscode.test-adapter-converter
-      # extensions.vscode-marketplace.vitest.explorer
+        # Testing
+        extensions.vscode-marketplace.hbenl.vscode-test-explorer
+        extensions.vscode-marketplace.ms-playwright.playwright
+        extensions.vscode-marketplace.ms-vscode.test-adapter-converter
+        # extensions.vscode-marketplace.vitest.explorer
 
-      # # Remote / SSH
-      # extensions.vscode-marketplace.ms-vscode-remote.remote-ssh
-      # extensions.vscode-marketplace.ms-vscode-remote.vscode-remote-extensionpack
+        # # Remote / SSH
+        # extensions.vscode-marketplace.ms-vscode-remote.remote-ssh
+        # extensions.vscode-marketplace.ms-vscode-remote.vscode-remote-extensionpack
 
-      # # Text to Speech
-      extensions.vscode-marketplace.ms-vscode.vscode-speech
-    ];
+        # # Text to Speech
+        # extensions.vscode-marketplace.ms-vscode.vscode-speech
+      ]
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "copilot-chat";
+          publisher = "github";
+          version = "0.14.1";
+          sha256 = "sha256-d5oohDNF44+3FRYOIAv32hSgFvvggugDP+kbOAcYfX0=";
+        }
+      ];
   };
 in {
   environment.systemPackages = with pkgs; [
