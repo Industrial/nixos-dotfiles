314c53dfa3fa8b369532bb41f7a05d91bbf09067
Tom Wieland <tom.wieland@gmail.com>
2024-05-24 20:50:12 +0200

    refactor(langhus,smithja): move settings into system config

--- hosts/langhus/default.nix
+++ hosts/langhus/default.nix
@@ -3,7 +3,18 @@
   specialArgs,
   ...
 }: let
-  settings = import ./settings.nix;
+  settings = {
+    hostname = "langhus";
+    stateVersion = "24.05";
+    system = "x86_64-linux";
+    hostPlatform = {
+      system = "x86_64-linux";
+    };
+    userdir = "/home/tom";
+    useremail = "tom.wieland@gmail.com";
+    userfullname = "Tom Wieland";
+    username = "tom";
+  };
 in
   inputs.nixpkgs.lib.nixosSystem {
     specialArgs =
--- hosts/smithja/default.nix
+++ hosts/smithja/default.nix
@@ -3,7 +3,19 @@
   specialArgs,
   ...
 }: let
-  settings = import ./settings.nix;
+  settings = {
+    hostname = "smithja";
+    stateVersion = 4;
+    system = "aarch64-darwin";
+    hostPlatform = {
+      config = "aarch64-apple-darwin";
+      system = "aarch64-darwin";
+    };
+    userdir = "/Users/twieland";
+    useremail = "twieland@suitsupply.com";
+    userfullname = "Tom Wieland";
+    username = "twieland";
+  };
 in
   inputs.nix-darwin.lib.darwinSystem {
     specialArgs =
