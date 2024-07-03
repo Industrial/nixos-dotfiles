f5cee3749f79ba5c3994ad745a5482bd7f1786ef
Tom Wieland <tom.wieland@gmail.com>
2024-07-03 15:35:56 +0000

    fix(test): run nix-unit in ci

--- flake.nix
+++ flake.nix
@@ -88,24 +88,23 @@
       in {
         formatter = pkgs.alejandra;
 
-        # # tests = import ./tests {inherit inputs;};
-        # checks = {
-        #   # tests =
-        #   #   inputs.nixpkgs.legacyPackages.${system}.runCommand "tests"
-        #   #   {
-        #   #     nativeBuildInputs = [inputs.nix-unit.packages.${system}.default];
-        #   #   } ''
-        #   #     #!/usr/bin/env bash
-        #   #     export HOME="$(realpath .)"
-        #   #     # The nix derivation must be able to find all used inputs in the
-        #   #     # nix-store because it cannot download it during buildTime.
-        #   #     nix-unit --eval-store "$HOME" \
-        #   #       --extra-experimental-features flakes \
-        #   #       --override-input nixpkgs ${inputs.nixpkgs} \
-        #   #       --flake ${self}#tests
-        #   #     touch $out
-        #   #   '';
-        # };
+        checks = {
+          tests =
+            inputs.nixpkgs.legacyPackages.${system}.runCommand "tests"
+            {
+              nativeBuildInputs = [inputs.nix-unit.packages.${system}.default];
+            } ''
+              #!/usr/bin/env bash
+              export HOME="$(realpath .)"
+              # The nix derivation must be able to find all used inputs in the
+              # nix-store because it cannot download it during buildTime.
+              nix-unit --eval-store "$HOME" \
+                --extra-experimental-features flakes \
+                --override-input nixpkgs ${inputs.nixpkgs} \
+                --flake ${self}#tests
+              touch $out
+            '';
+        };
       };
     };
 }
