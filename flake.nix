4bb2d48c6fffd1645c319966b10d6478daf8e0b6
Tom Wieland <tom.wieland@gmail.com>
2024-07-25 20:54:05 +0000

    refactor(flake): remove flake-parts and just use genAttrs

--- flake.nix
+++ flake.nix
@@ -3,9 +3,6 @@
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
-    # Flake Parts
-    flake-parts.url = "github:hercules-ci/flake-parts";
-
     # Nix Git Hooks
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
@@ -17,7 +14,6 @@
     # Nix Unit
     nix-unit.url = "github:nix-community/nix-unit";
     nix-unit.inputs.nixpkgs.follows = "nixpkgs";
-    nix-unit.inputs.flake-parts.follows = "flake-parts";
 
     # Nix Darwin
     nix-darwin.url = "github:LnL7/nix-darwin";
@@ -44,69 +40,58 @@
     cryptpad.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = inputs @ {self, ...}: let
-    githubActionsSystems = ["x86_64-linux" "x86_64-darwin"];
-  in
-    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = inputs.nixpkgs.lib.systems.flakeExposed;
-      flake = {
-        tests = let
-          settings = {
-            hostname = "testhostname";
-            stateVersion = "24.05";
-            system = "x86_64-linux";
-            hostPlatform = {
-              system = "x86_64-linux";
-            };
-            userdir = "/Users/test";
-            useremail = "test@test.com";
-            userfullname = "Chadster McChaddington";
-            username = "test";
-          };
-        in {
-          features = import ./features/tests.nix {
-            inherit inputs settings;
-            pkgs = import inputs.nixpkgs {
-              # TODO: How can I abstract away the system with flake-parts?
-              system = "x86_64-linux";
-              config = {
-                allowUnfree = true;
-              };
-            };
-          };
+    forAllSystems = inputs.nixpkgs.lib.genAttrs inputs.nixpkgs.lib.systems.flakeExposed;
+  in {
+    tests = let
+      settings = {
+        hostname = "testhostname";
+        stateVersion = "24.05";
+        system = "x86_64-linux";
+        hostPlatform = {
+          system = "x86_64-linux";
         };
-
-        # Creates a GitHub Actions matrix for the checks.
-        githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
-          # Only run checks on the systems that we support on GitHub Actions.
-          checks = inputs.nixpkgs.lib.getAttrs githubActionsSystems self.checks;
-        };
-
-        nixosConfigurations = {} // (import ./hosts/langhus.nix {inherit inputs;});
-        darwinConfigurations = {} // (import ./hosts/smithja.nix {inherit inputs;});
+        userdir = "/Users/test";
+        useremail = "test@test.com";
+        userfullname = "Chadster McChaddington";
+        username = "test";
       };
-
-      perSystem = {
-        system,
-        pkgs,
-        ...
-      }: {
-        formatter = pkgs.alejandra;
-
-        checks = {
-          pre-commit-check = import ./checks/pre-commit.nix {inherit inputs system pkgs;};
-          tests = import ./checks/tests.nix {inherit inputs self system pkgs;};
-        };
-
-        devShells = {
-          default = pkgs.mkShell {
-            inherit (self.checks.${system}.pre-commit-check) shellHook;
-            buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
-            inputsFrom = [];
-            packages = with pkgs; [
-              direnv
-            ];
+    in {
+      features = import ./features/tests.nix {
+        inherit inputs settings;
+        pkgs = import inputs.nixpkgs {
+          system = "x86_64-linux";
+          config = {
+            allowUnfree = true;
           };
         };
       };
     };
+
+    githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
+      checks = inputs.nixpkgs.lib.getAttrs ["x86_64-linux" "aarch64-darwin"] self.checks;
+    };
+
+    nixosConfigurations = {} // (import ./hosts/langhus.nix {inherit inputs;});
+    darwinConfigurations = {} // (import ./hosts/smithja.nix {inherit inputs;});
+
+    checks = forAllSystems (system: let
+      pkgs = import inputs.nixpkgs {inherit system;};
+    in {
+      pre-commit-check = import ./checks/pre-commit.nix {inherit inputs system pkgs;};
+      tests = import ./checks/tests.nix {inherit inputs self system pkgs;};
+    });
+
+    devShells = forAllSystems (system: let
+      pkgs = import inputs.nixpkgs {inherit system;};
+    in {
+      default = pkgs.mkShell {
+        inherit (self.checks.${system}.pre-commit-check) shellHook;
+        buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+        inputsFrom = [];
+        packages = with pkgs; [
+          direnv
+        ];
+      };
+    });
+  };
 }
