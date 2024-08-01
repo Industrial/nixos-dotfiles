25079d9285759c56fa96e89e17a0f43762b6b781
Tom Wieland <tom.wieland@gmail.com>
2024-08-01 19:22:09 +0000

    fix(tests): export github actions

--- github-actions.nix
+++ github-actions.nix
@@ -2,7 +2,8 @@
   inputs,
   self,
   ...
-}:
-inputs.nix-github-actions.lib.mkGithubMatrix {
-  checks = inputs.nixpkgs.lib.getAttrs ["x86_64-linux" "aarch64-darwin"] self.checks;
+}: {
+  matrix = inputs.nix-github-actions.lib.mkGithubMatrix {
+    checks = inputs.nixpkgs.lib.getAttrs ["x86_64-linux" "aarch64-darwin"] self.checks;
+  };
 }
