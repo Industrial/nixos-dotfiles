88583d0947e3bc27e0557d61ea4b32def925c751
Tom Wieland <tom.wieland@gmail.com>
2024-08-03 15:35:40 +0000

    refactor: remove bitwarden

--- features/security/tests.nix
+++ features/security/tests.nix
@@ -1,5 +1,4 @@
 args @ {...}: {
-  bitwarden = import ./bitwarden/tests.nix args;
   vaultwarden = import ./vaultwarden/tests.nix args;
   veracrypt = import ./veracrypt/tests.nix args;
   yubikey-manager = import ./yubikey-manager/tests.nix args;
