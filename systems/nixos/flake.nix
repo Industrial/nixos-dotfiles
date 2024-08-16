5e0713facc11cae709d8493cd6fab5eef3efbb74
Tom Wieland <tom.wieland@gmail.com>
2024-08-16 08:37:56 +0200

    feat(systems): add huginn host to nixos system

--- systems/nixos/flake.nix
+++ systems/nixos/flake.nix
@@ -5,6 +5,8 @@
     nix-vscode-extensions.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = inputs @ {...}: {
-    nixosConfigurations = {} // (import ../../hosts/langhus.nix {inherit inputs;});
+    nixosConfigurations = {}
+    // (import ../../hosts/langhus.nix {inherit inputs;})
+    // (import ../../hosts/huginn.nix {inherit inputs;});
   };
 }
