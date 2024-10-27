2e4dce0dcde1f5a250e7e3305bd79666482c4e32
tom <tom@x86_64-linux.local>
2024-10-27 10:20:00 +0000

    refactor(nix): removed nix.package setting

--- features/nix/default.nix
+++ features/nix/default.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   system.stateVersion = settings.stateVersion;
-  nix.package = pkgs.nixFlakes;
+  #nix.package = pkgs.nixVersions.stable;
   nix.settings.experimental-features = "nix-command flakes";
   nix.settings.allow-import-from-derivation = true;
 }
