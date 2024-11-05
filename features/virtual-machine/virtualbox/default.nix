f0883dc2b6c40462b9d0c87b60a65d37f820330c
tom <tom@x86_64-linux.local>
2024-11-05 08:01:08 +0000

    feat(virtualbox): extra settings

--- features/virtual-machine/virtualbox/default.nix
+++ features/virtual-machine/virtualbox/default.nix
@@ -1,11 +1,26 @@
-{pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    virtualbox
-  ];
+{
+  settings,
+  pkgs,
+  ...
+}: {
+  # Don't install virtualbox to systemPackages. It will make it unusable.
+  # Instead use the options below.
   virtualisation = {
     virtualbox = {
       host = {
         enable = true;
+        enableExtensionPack = true;
+      };
+      guest = {
+        enable = true;
+        dragAndDrop = true;
+      };
+    };
+  };
+  users = {
+    extraGroups = {
+      vboxusers = {
+        members = [settings.userfullname];
       };
     };
   };
