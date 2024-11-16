067e59ff4653796d934d6a610009852cebfce890
tom <tom@x86_64-linux.local>
2024-11-16 00:25:15 +0000

    Revert "refactor(hosts/drakker): remove nvidia config"
    This reverts commit f0491211ccdac4412686c79193cafd5590de4c03.

--- hosts/drakkar.nix
+++ hosts/drakkar.nix
@@ -173,6 +173,52 @@ in {
           device = "/dev/disk/by-uuid/10921065-05ea-4be2-b499-c19a736e3f33";
           fsType = "ext4";
         };
+
+        # Graphics
+        hardware = {
+          graphics = {
+            enable = true;
+          };
+          nvidia = {
+            # Modesetting is required.
+            modesetting = {
+              enable = true;
+            };
+
+            powerManagement = {
+              # Nvidia power management. Experimental, and can cause
+              # sleep/suspend to fail. Enable this if you have graphical
+              # corruption issues or application crashes after waking up from
+              # sleep. This fixes it by saving the entire VRAM memory to /tmp/
+              # instead of just the bare essentials.
+              enable = true;
+
+              # Fine-grained power management. Turns off GPU when not in use.
+              # Experimental and only works on modern Nvidia GPUs (Turing or
+              # newer).
+              finegrained = false;
+            };
+
+            # Use the NVidia open source kernel module (not to be confused
+            # with the independent third-party "nouveau" open source driver).
+            # Support is limited to the Turing and later architectures. Full
+            # list of supported GPUs is at:
+            # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+            # Only available from driver 515.43.04+ Currently
+            # alpha-quality/buggy, so false is currently the recommended
+            # setting.
+            open = false;
+
+            # Enable the Nvidia settings menu, accessible via
+            # `nvidia-settings`.
+            nvidiaSettings = true;
+          };
+        };
+        services = {
+          xserver = {
+            videoDrivers = ["nvidia"];
+          };
+        };
       }
     ];
   };
