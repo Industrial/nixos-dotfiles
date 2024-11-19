4f8a43f4a1364b2d1036ae8839058c2810b2aceb
ss-twieland <twieland@suitsupply.com>
2024-11-19 12:25:42 +0100

    feat(tailscale): for linux set the firewall trusted interfaces for tailscale

--- features/security/tailscale/default.nix
+++ features/security/tailscale/default.nix
@@ -1,7 +1,16 @@
-{...}: {
+{stdenv, ...}: {
   services = {
     tailscale = {
       enable = true;
     };
   };
+
+  networking =
+    if stdenv.isLinux
+    then {
+      firewall = {
+        trustedInterfaces = ["tailscale0"];
+      };
+    }
+    else {};
 }
