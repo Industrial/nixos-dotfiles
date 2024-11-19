4f94b0e308e3463c82e918a12e3c601e74317e1f
tom <tom@x86_64-linux.local>
2024-11-19 23:44:40 +0000

    fix(searx): set server settings

--- features/network/searx/default.nix
+++ features/network/searx/default.nix
@@ -2,6 +2,13 @@
   services = {
     searx = {
       enable = true;
+      settings = {
+        server = {
+          port = 4001;
+          bind_address = "0.0.0.0";
+          secret_key = "keyboardcat";
+        };
+      };
     };
   };
 }
