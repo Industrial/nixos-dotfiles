82a7d69bf5e55cc1a6e017e844a42dace75a324a
tom <tom@x86_64-linux.local>
2024-12-01 16:47:24 +0000

    fix(plex): add plex user and add it to group data

--- features/media/plex/default.nix
+++ features/media/plex/default.nix
@@ -6,6 +6,23 @@
       dataDir = "/run/media/tom/Data/Videos";
       openFirewall = true;
       group = "data";
+      user = "plex";
+    };
+  };
+
+  users = {
+    users = {
+      plex = {
+        isSystemUser = false;
+        home = "/home/plex";
+        createHome = false;
+        group = "plex";
+        extraGroups = ["data"];
+      };
+    };
+
+    groups = {
+      plex = {};
     };
   };
 }
