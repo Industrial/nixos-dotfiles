0de7a6d3c5d0b22352c0d55cc54e8ddd58625f15
tom <tom@x86_64-linux.local>
2024-12-18 19:09:53 +0000

    chore(dns): remove commented out sections in dns.nix for clarity and maintenance

--- features/nixos/networking/dns.nix
+++ features/nixos/networking/dns.nix
@@ -21,12 +21,6 @@
             minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
           };
         };
-
-        # If you want, choose a specific set of servers that come from your sources.
-        # Here it's from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
-        # If you don't specify any, dnscrypt-proxy will automatically rank servers
-        # that match your criteria and choose the best one.
-        # server_names = [ ... ];
       };
     };
   };
@@ -36,10 +30,6 @@
       dnscrypt-proxy2 = {
         serviceConfig = {
           StateDirectory = "dnscrypt-proxy";
-          # If you're trying to set up persistence with dnscrypt-proxy2 and it isn't working
-          # because of permission issues, try the following:
-          # StateDirectory = lib.mkForce "";
-          # ReadWritePaths = "/var/lib/dnscrypt-proxy"; # Cache directory for dnscrypt-proxy2, persist this
         };
       };
     };
