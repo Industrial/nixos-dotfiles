7154480ba372a96f4312dd0ab7d1c63bb0e06dbf
tom <tom@x86_64-linux.local>
2024-12-29 10:41:04 +0000

    refactor(gnome): remove unused gnome tools and update configuration

--- features/window-manager/gnome/default.nix
+++ features/window-manager/gnome/default.nix
@@ -35,51 +35,25 @@
   environment = {
     systemPackages = with pkgs; [
       # - Media
-      #   - Audio
-      gnome-music
-      gnome-sound-recorder
-      spot
-      totem
-      #   - Bittorrent
-      fragments
       #   - Documents
       evince
       #   - Images
       eog
-      gnome-photos
       gnome-screenshot
       shotwell
-      #   - Video
-      clapper
 
       # - Disks
       gnome-disk-utility
       gparted
 
       # - Security
-      authenticator
       gnome-keyring
-      gnome-secrets
 
       # - Office
       dialect
       file-roller
       gnome-calculator
-      gnome-calendar
-      wordbook # gnome-dictionary
       gnome-font-viewer
-
-      # - Email
-      # Email client.
-      geary
-
-      # - Development
-      # Git GUI.
-      gitg
-
-      # - Games
-      gnome-chess
-      gnome-sudoku
 
       # - System
       # Edit Gnome / Dconf settings.
@@ -90,26 +64,16 @@
 
       gnome-console
       gnome-decoder
-      gnome-graphs
       gnome-tweaks
       gnome-bluetooth
       gnome-characters
       gnome-clocks
-      gnome-color-manager
-      gnome-contacts
       gnome-control-center
-      gnome-logs
-      gnome-maps
-      gnome-nettool
       gnome-panel
       gnome-session
       gnome-shell
       gnome-shell-extensions
-      gnome-weather
       nautilus
-      papers
-      seahorse
-      wayfarer
 
       # - Tiling Window Manager
       gnomeExtensions.pop-shell
