dade8c66cac400b64813cba9e17996bf8449bd38
tom <tom@x86_64-linux.local>
2025-02-22 23:08:08 +0000

    feat(sudo): disable timeout on sudo to enhance usability with security keys

--- features/nixos/security/sudo/default.nix
+++ features/nixos/security/sudo/default.nix
@@ -4,6 +4,10 @@
       enable = true;
       wheelNeedsPassword = true;
       execWheelOnly = true;
+      # Disable the timeout on sudo. I'm sick of pressing my security keys.
+      extraConfig = ''
+        Defaults        timestamp_timeout=-1
+      '';
     };
   };
 }
