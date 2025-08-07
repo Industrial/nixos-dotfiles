e7c9e23956ae82f7182a9b416d31087bf228a1c6
tom <tom.wieland@gmail.com>
2025-08-07 13:34:24 +0200

    chore(nixos): comment out TCP memory settings for network performance optimization

--- features/nixos/boot/default.nix
+++ features/nixos/boot/default.nix
@@ -62,8 +62,8 @@
       # Network performance optimization
       "net.core.rmem_max=16777216"
       "net.core.wmem_max=16777216"
-      "net.ipv4.tcp_rmem=4096 87380 16777216"
-      "net.ipv4.tcp_wmem=4096 65536 16777216"
+      # "net.ipv4.tcp_rmem=4096 87380 16777216"
+      # "net.ipv4.tcp_wmem=4096 65536 16777216"
       "net.core.netdev_max_backlog=5000"
       "net.ipv4.tcp_congestion_control=bbr"
 
