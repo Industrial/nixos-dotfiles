0b0ed142b5c8526573e52fb1bd478403591fe432
Tom Wieland <tom.wieland@gmail.com>
2025-10-16 08:06:15 +0200

    fix: mark broken GIMP plugins in default.nix
    Comment out gimpPlugins.fourier and gimpPlugins.resynthesizer in default.nix as they are marked as broken in nixpkgs, ensuring a cleaner configuration and preventing potential issues during package installation.

--- features/creative/gimp/default.nix
+++ features/creative/gimp/default.nix
@@ -3,8 +3,8 @@
     gimp
     gimp-with-plugins
     gimpPlugins.gmic
-    gimpPlugins.fourier
-    gimpPlugins.resynthesizer
+    # gimpPlugins.fourier  # Marked as broken in nixpkgs
+    # gimpPlugins.resynthesizer  # Marked as broken in nixpkgs
     gimpPlugins.lightning
   ];
 }
