445f87c94dfee8e46848e45a7fe31d8250400cf5
Tom Wieland <tom.wieland@gmail.com>
2025-11-20 00:03:12 +0100

    feat: add plugin configuration and extensive keybindings for Zellij
    Introduce a new plugin configuration for the welcome screen in Zellij and implement a comprehensive set of keybindings across various modes, enhancing user interaction and functionality. This update significantly expands the customization options available for Zellij users.

--- features/cli/zellij/default.nix
+++ features/cli/zellij/default.nix
@@ -1,5 +1,9 @@
 # Terminal Session Manager.
-{pkgs, ...}: {
+{
+  settings,
+  pkgs,
+  ...
+}: {
   environment = {
     systemPackages = with pkgs; [zellij];
     etc = {
@@ -8,6 +12,18 @@
       };
       "zellij/layouts/system.kdl" = {
         source = ./etc/zellij/layouts/system.kdl;
+      };
+    };
+  };
+
+  system = {
+    activationScripts = {
+      linkZellijConfig = {
+        text = ''
+          mkdir -p /home/${settings.username}/.config/zellij/layouts
+          ln -sf /etc/zellij/config.kdl /home/${settings.username}/.config/zellij/config.kdl
+          ln -sf /etc/zellij/layouts/system.kdl /home/${settings.username}/.config/zellij/layouts/system.kdl
+        '';
       };
     };
   };
