4adcd22b96e04fbd4b78d31ea1020d2840943562
Tom Wieland <tom.wieland@gmail.com>
2025-11-19 23:51:16 +0100

    refactor: streamline fish configuration in default.nix
    Revise the fish configuration in default.nix by removing the use of lib.mkForce and restructuring the session variables. This update enhances clarity and organization, ensuring a more efficient setup for the fish shell environment while maintaining the same functionality.

--- features/cli/fish/default.nix
+++ features/cli/fish/default.nix
@@ -1,7 +1,6 @@
 {
   settings,
   pkgs,
-  lib,
   ...
 }: let
   havamalPlugin = pkgs.callPackage ./havamal.nix {inherit settings pkgs;};
@@ -9,55 +8,55 @@ in {
   programs = {
     fish = {
       enable = true;
+      interactiveShellInit = ''
+        # Disable greeting
+        function fish_greeting
+        end
+
+        # Enable Fish plugins
+        source ${havamalPlugin}/share/fish/vendor_conf.d/Hávamál.fish
+
+        # Add local bin to PATH
+        set -x PATH "$PATH:$HOME/.local/bin"
+
+        # Use vim keybindings.
+        fish_vi_key_bindings
+
+        # Remove default aliases.
+        functions -e l
+        functions -e ll
+        functions -e ls
+
+        # Keybindings
+        function fish_user_key_bindings
+          bind --user -M insert \cp up-or-search
+          bind --user -M insert \cn down-or-search
+        end
+
+        # Direnv
+        direnv hook fish | source
+
+        # Starship Shell
+        starship init fish | source
+      '';
     };
   };
+
   environment = {
     shells = with pkgs; [
       fish
     ];
-    etc = {
-      "fish/config.fish" = lib.mkForce {
-        text = ''
-          # Disable greeting
-          function fish_greeting
-          end
-
-          # Enable Fish plugins
-          source ${havamalPlugin}/share/fish/vendor_conf.d/Hávamál.fish
-
-          # Variables
-          set -x EDITOR "vnim"
-          set -x GIT_EDITOR "nvim"
-          set -x DIFFPROG "nvim -d"
-          set -x PATH "$PATH:$HOME/.local/bin"
-          set -x XDG_CACHE_HOME "$HOME/.cache"
-          set -x XDG_CONFIG_HOME "$HOME/.config"
-          set -x XDG_DATA_HOME "$HOME/.local/share"
-          set -x XDG_STATE_HOME "$HOME/.local/state"
-
-          # Use vim keybindings.
-          fish_vi_key_bindings
-
-          # Remove default aliases.
-          functions -e l
-          functions -e ll
-          functions -e ls
-
-          # Keybindings
-          function fish_user_key_bindings
-            bind --user -M insert \cp up-or-search
-            bind --user -M insert \cn down-or-search
-          end
-
-          # Direnv
-          direnv hook fish | source
-
-          # Starship Shell
-          starship init fish | source
-        '';
-      };
+    sessionVariables = {
+      EDITOR = "vnim";
+      GIT_EDITOR = "nvim";
+      DIFFPROG = "nvim -d";
+      XDG_CACHE_HOME = "$HOME/.cache";
+      XDG_CONFIG_HOME = "$HOME/.config";
+      XDG_DATA_HOME = "$HOME/.local/share";
+      XDG_STATE_HOME = "$HOME/.local/state";
     };
   };
+
   users = {
     users = {
       "${settings.username}" = {
