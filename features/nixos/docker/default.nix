965c294e2637eafc60d0bf27e55c3486266bac54
tom <tom@x86_64-linux.local>
2025-12-16 09:44:26 +0000

    feat: enable rootless Docker configuration and adjust kernel settings
    - Added support for rootless Docker daemon with customizable settings including DNS and registry mirrors.
    - Configured kernel sysctl settings to allow unprivileged users to bind to port 80.

--- features/nixos/docker/default.nix
+++ features/nixos/docker/default.nix
@@ -4,6 +4,16 @@
       enable = true;
       # Enable Docker daemon on boot
       enableOnBoot = true;
+
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+        # Optionally customize rootless Docker daemon settings
+        daemon.settings = {
+          dns = ["1.1.1.1" "8.8.8.8"];
+          registry-mirrors = ["https://mirror.gcr.io"];
+        };
+      };
     };
   };
 
@@ -27,6 +37,14 @@
           # Ensure docker group has access to socket
           SupplementaryGroups = ["docker"];
         };
+      };
+    };
+  };
+
+  boot = {
+    kernel = {
+      sysctl = {
+        "net.ipv4.ip_unprivileged_port_start" = 80;
       };
     };
   };
