733b25a1fd0ba1d6603ded15ce8a4d23e15b4264
tom <tom@x86_64-linux.local>
2026-01-25 19:44:42 +0000

    Update zellij configuration and layouts

--- features/cli/zellij/default.nix
+++ features/cli/zellij/default.nix
@@ -3,7 +3,12 @@
   settings,
   pkgs,
   ...
-}: {
+}: let
+  zjstatus-hints = pkgs.fetchurl {
+    url = "https://github.com/b0o/zjstatus-hints/releases/latest/download/zjstatus-hints.wasm";
+    hash = "sha256-k2xV6QJcDtvUNCE4PvwVG9/ceOkk+Wa/6efGgr7IcZ0=";
+  };
+in {
   environment = {
     systemPackages = with pkgs; [
       zellij
@@ -16,6 +21,9 @@
       "zellij/layouts/system.kdl" = {
         source = ./etc/zellij/layouts/system.kdl;
       };
+      "zellij/plugins/zjstatus-hints.wasm" = {
+        source = zjstatus-hints;
+      };
     };
   };
 
@@ -24,8 +32,10 @@
       linkZellijConfig = {
         text = ''
           mkdir -p /home/${settings.username}/.config/zellij/layouts
+          mkdir -p /home/${settings.username}/.config/zellij/plugins
           ln -sf /etc/zellij/config.kdl /home/${settings.username}/.config/zellij/config.kdl
           ln -sf /etc/zellij/layouts/system.kdl /home/${settings.username}/.config/zellij/layouts/system.kdl
+          ln -sf /etc/zellij/plugins/zjstatus-hints.wasm /home/${settings.username}/.config/zellij/plugins/zjstatus-hints.wasm
         '';
       };
     };
