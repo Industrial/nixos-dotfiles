9920b8c9f2da15a9ab16a4d95bb21022cbb728c2
Tom Wieland <tom.wieland@gmail.com>
2026-05-20 14:57:52 +0200

    fix(nixos): correct devenv.cachix.org trusted-public-key
    Update substituter key to match current devenv binary cache signing key.

--- features/nixos/default.nix
+++ features/nixos/default.nix
@@ -24,7 +24,8 @@
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7BKqP7YhK1i3JvGvscqg5k="
-        "devenv.cachix.org-1:w1cLUi8dv3hgsSP+aeo2H8R7ExhwCQrC19yFK+ZZmoc="
+        # https://devenv.sh/binary-caching/
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       ];
 
       # Enable experimental features for better performance
