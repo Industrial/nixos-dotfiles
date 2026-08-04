0210f5ea1f30f691e49ac1d18a4b2e9922cb1484
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:17 +0200

    test(lmstudio): colocate assay suite for systemPackages

---
+++ features/ai/lmstudio/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { lmstudio = "lmstudio"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "lmstudio" {
+    systemPackages = assay.eq packages ''[ "lmstudio" ]'';
+  }
