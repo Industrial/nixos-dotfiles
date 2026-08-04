366a259110407f69c906062bc70d8d28eea759d1
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:20 +0200

    test(procs): colocate assay suite for systemPackages

---
+++ features/cli/procs/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { procs = "procs"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "procs" {
+    systemPackages = assay.eq packages ''[ "procs" ]'';
+  }
