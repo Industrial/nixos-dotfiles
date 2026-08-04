c4f02fe0131fa5b4e0f43e3402f8619c39f9712a
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 10:07:28 +0200

    style(assay): nixfmt mobile colocated suites

--- features/mobile/android-studio/default.assay.nix
+++ features/mobile/android-studio/default.assay.nix
@@ -1,10 +1,12 @@
 # Colocated suite: systemPackages from stubbed pkgs.
 let
   assay = import ./../../../common/assay/default.nix;
-  pkgs = { "android-studio" = "android-studio"; "android-tools" = "android-tools"; };
-  mod = import ./default.nix { inherit pkgs; };
-
+  pkgs = {
+    "android-studio" = "android-studio";
+    "android-tools" = "android-tools";
+  };
+  mod = import ./default.nix {inherit pkgs;};
 in
   assay.suite "android-studio" {
-    systemPackages = assay.eq mod.environment.systemPackages [ "android-studio" "android-tools" ];
+    systemPackages = assay.eq mod.environment.systemPackages ["android-studio" "android-tools"];
   }
