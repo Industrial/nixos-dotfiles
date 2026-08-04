753303425d0a37d7b336658c3d68195253ce99bc
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:28 +0200

    test(slock): colocate assay suite for systemPackages

---
+++ features/window-manager/slock/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { slock = "slock"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "slock" {
+    systemPackages = assay.eq packages ''[ "slock" ]'';
+  }
