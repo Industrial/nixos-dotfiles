fe3de06fcd08430b9c67214651cfb736ffa30197
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:25 +0200

    test(android-studio): colocate assay suite for systemPackages

---
+++ features/mobile/android-studio/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { "android-studio" = "android-studio"; "android-tools" = "android-tools"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "android-studio" {
+    systemPackages = assay.eq packages ''[ "android-studio" "android-tools" ]'';
+  }
