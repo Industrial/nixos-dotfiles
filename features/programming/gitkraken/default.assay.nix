0572d8cd4bc485cbb8667a411c4a2abc30c7597c
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:26 +0200

    test(gitkraken): colocate assay suite for systemPackages

---
+++ features/programming/gitkraken/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { gitkraken = "gitkraken"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "gitkraken" {
+    systemPackages = assay.eq packages ''[ "gitkraken" ]'';
+  }
