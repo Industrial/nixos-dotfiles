aa1c311fc10190193e0ee83e94fc6b32e627c00a
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:26 +0200

    test(meld): colocate assay suite for systemPackages

---
+++ features/programming/meld/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { meld = "meld"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "meld" {
+    systemPackages = assay.eq packages ''[ "meld" ]'';
+  }
