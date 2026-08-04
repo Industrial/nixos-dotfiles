e8ff3a3166eeb31cafa966969376ab25f0c52d19
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:36:08 +0200

    test(gpg): colocate assay suite

---
+++ features/cli/gpg/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: gnupg agent enable + packages.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  mod = ''
+    (let
+      pkgs = { gnupg = "gnupg"; pinentry-all = "pinentry-all"; };
+    in import ${modFile} { inherit pkgs; })
+  '';
+in
+  assay.suite "gpg" {
+    packages = assay.eq "${mod}.environment.systemPackages" ''[ "gnupg" "pinentry-all" ]'';
+    agent = assay.eq "${mod}.programs.gnupg.agent.enable" "true";
+  }
