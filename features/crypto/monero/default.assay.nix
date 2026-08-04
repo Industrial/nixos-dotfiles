a6127c4bb6f4e677e67253bce4884919b1604123
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:23 +0200

    test(monero): colocate assay suite for systemPackages

---
+++ features/crypto/monero/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { "monero-cli" = "monero-cli"; "monero-gui" = "monero-gui"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "monero" {
+    systemPackages = assay.eq packages ''[ "monero-cli" "monero-gui" ]'';
+  }
