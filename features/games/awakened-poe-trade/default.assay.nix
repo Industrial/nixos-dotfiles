ab364ec06d1301ce14bfec4d430908be77ed3273
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 10:07:06 +0200

    style(assay): nixfmt games colocated suites

--- features/games/awakened-poe-trade/default.assay.nix
+++ features/games/awakened-poe-trade/default.assay.nix
@@ -1,10 +1,9 @@
 # Colocated suite: systemPackages from stubbed pkgs.
 let
   assay = import ./../../../common/assay/default.nix;
-  pkgs = { "awakened-poe-trade" = "awakened-poe-trade"; };
-  mod = import ./default.nix { inherit pkgs; };
-
+  pkgs = {"awakened-poe-trade" = "awakened-poe-trade";};
+  mod = import ./default.nix {inherit pkgs;};
 in
   assay.suite "awakened-poe-trade" {
-    systemPackages = assay.eq mod.environment.systemPackages [ "awakened-poe-trade" ];
+    systemPackages = assay.eq mod.environment.systemPackages ["awakened-poe-trade"];
   }
--- features/games/lutris/default.assay.nix
+++ features/games/lutris/default.assay.nix
@@ -1,8 +1,8 @@
 # Colocated suite: programs.gamemode.enable.
 let
   assay = import ./../../../common/assay/default.nix;
-  pkgs = { };
-  mod = import ./default.nix { inherit pkgs; };
+  pkgs = {};
+  mod = import ./default.nix {inherit pkgs;};
 in
   assay.suite "lutris" {
     enabled = assay.eq mod.programs.gamemode.enable true;
