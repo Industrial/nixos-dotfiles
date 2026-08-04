a37b64a84750a76aee8ac0a9aaa7aded1bed320d
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:16 +0200

    test(litellm): colocate assay suite for systemPackages

---
+++ features/ai/litellm/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { litellm = "litellm"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "litellm" {
+    systemPackages = assay.eq packages ''[ "litellm" ]'';
+  }
