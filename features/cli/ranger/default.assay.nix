bae4668302fe5d4fa2f7ccd022bab12da74bc5ca
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:20 +0200

    test(ranger): colocate assay suite for systemPackages

---
+++ features/cli/ranger/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { ranger = "ranger"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "ranger" {
+    systemPackages = assay.eq packages ''[ "ranger" ]'';
+  }
