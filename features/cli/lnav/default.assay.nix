82430f69ced72e76bba4da0ef3d74d2388534e43
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:19 +0200

    test(lnav): colocate assay suite for systemPackages

---
+++ features/cli/lnav/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { lnav = "lnav"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "lnav" {
+    systemPackages = assay.eq packages ''[ "lnav" ]'';
+  }
