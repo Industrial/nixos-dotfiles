0cf12b688bbf72267268330657e0e95ee5b8647a
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:23 +0200

    test(obs-studio): colocate assay suite for systemPackages

---
+++ features/creative/obs-studio/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { "v4l-utils" = "v4l-utils"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "obs-studio" {
+    systemPackages = assay.eq packages ''[ "v4l-utils" ]'';
+  }
