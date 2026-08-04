52c2443a0538b01a2cb6e8bd9cbe95e167d2f866
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:17 +0200

    test(bandwhich): colocate assay suite for systemPackages

---
+++ features/cli/bandwhich/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { bandwhich = "bandwhich"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "bandwhich" {
+    systemPackages = assay.eq packages ''[ "bandwhich" ]'';
+  }
