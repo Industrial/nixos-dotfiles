c4fe0e9e361ed06eeae8251953f22d37527e292e
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:36:03 +0200

    test(lean-ctx): colocate assay suite

---
+++ features/ai/lean-ctx/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: local callPackage lands on systemPackages.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+(    let
+      pkgs = { callPackage = path: args: "local-pkg"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages)
+'';
+in
+  assay.suite "lean-ctx" {
+    systemPackages = assay.eq packages ''[ "local-pkg" ]'';
+  }
