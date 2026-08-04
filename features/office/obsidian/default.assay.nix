aada5e1dd9908a6ac138f388f7c48827a7d49016
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:25 +0200

    test(obsidian): colocate assay suite for systemPackages

---
+++ features/office/obsidian/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { obsidian = "obsidian"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "obsidian" {
+    systemPackages = assay.eq packages ''[ "obsidian" ]'';
+  }
