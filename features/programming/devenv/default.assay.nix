0ccadfd39f0292ef3b102cbfd7c0dd80bf1d19d2
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:26 +0200

    test(devenv): colocate assay suite for systemPackages

---
+++ features/programming/devenv/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { devenv = "devenv"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "devenv" {
+    systemPackages = assay.eq packages ''[ "devenv" ]'';
+  }
