5cca18b1f3cba524bc99122063b2e8941ee7b561
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:20 +0200

    test(p7zip): colocate assay suite for systemPackages

---
+++ features/cli/p7zip/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { p7zip = "p7zip"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "p7zip" {
+    systemPackages = assay.eq packages ''[ "p7zip" ]'';
+  }
