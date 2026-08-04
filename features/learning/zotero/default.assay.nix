b747008a598cfea7622e718f77b17226f819b4d3
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 10:07:11 +0200

    style(assay): nixfmt learning colocated suites

--- features/learning/zotero/default.assay.nix
+++ features/learning/zotero/default.assay.nix
@@ -1,10 +1,9 @@
 # Colocated suite: systemPackages from stubbed pkgs.
 let
   assay = import ./../../../common/assay/default.nix;
-  pkgs = { zotero = "zotero"; };
-  mod = import ./default.nix { inherit pkgs; };
-
+  pkgs = {zotero = "zotero";};
+  mod = import ./default.nix {inherit pkgs;};
 in
   assay.suite "zotero" {
-    systemPackages = assay.eq mod.environment.systemPackages [ "zotero" ];
+    systemPackages = assay.eq mod.environment.systemPackages ["zotero"];
   }
