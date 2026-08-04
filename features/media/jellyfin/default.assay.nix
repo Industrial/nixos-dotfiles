93da800cbaba3350a0ed27a3103536b37cfe75d2
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:36:12 +0200

    test(jellyfin): colocate assay suite

---
+++ features/media/jellyfin/default.assay.nix
@@ -0,0 +1,25 @@
+# Colocated suite: module top-level shape with stubbed args.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  expr = ''
+    (import ${modFile} {
+      settings = { hostname = "testhost"; username = "alice"; useremail = "a@b.c"; };
+      pkgs = {
+        callPackage = path: args: "pkg";
+        stdenv = {
+          mkDerivation = args: args.name or args.pname or "drv";
+          hostPlatform = { system = "x86_64-linux"; };
+        };
+        fish = "fish";
+        writeShellScript = name: text: name;
+        writeText = name: text: name;
+      };
+      lib = (import <nixpkgs> {}).lib;
+      config = { };
+    })
+  '';
+in
+  assay.suite "jellyfin" {
+    shape = assay.hasAttrs expr [ "services" "systemd" "users" ];
+  }
