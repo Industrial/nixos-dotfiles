34f9832520e50d5aa62e2acd03a027157621e1b4
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:24 +0200

    test(anki): colocate assay suite for systemPackages

---
+++ features/learning/anki/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { anki = "anki"; "anki-bin" = "anki-bin"; "anki-sync-server" = "anki-sync-server"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "anki" {
+    systemPackages = assay.eq packages ''[ "anki" "anki-bin" "anki-sync-server" ]'';
+  }
