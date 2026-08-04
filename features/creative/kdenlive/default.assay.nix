863e5ec071ff97351f8b0e1a6f25a598eb04cba1
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:23 +0200

    test(kdenlive): colocate assay suite for systemPackages

---
+++ features/creative/kdenlive/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { kdePackages = { kdenlive = "kdePackages.kdenlive"; }; ffmpeg = "ffmpeg"; mediainfo = "mediainfo"; mkvtoolnix = "mkvtoolnix"; handbrake = "handbrake"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "kdenlive" {
+    systemPackages = assay.eq packages ''[ "kdePackages.kdenlive" "ffmpeg" "mediainfo" "mkvtoolnix" "handbrake" ]'';
+  }
