4b2b87e6ebe6f1cf42cee6e0bdd98e86b353c416
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:19 +0200

    test(gping): colocate assay suite for systemPackages

---
+++ features/cli/gping/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { gping = "gping"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "gping" {
+    systemPackages = assay.eq packages ''[ "gping" ]'';
+  }
