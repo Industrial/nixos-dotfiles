10d32d13d77320afe5f937e3348bb462c816cd5b
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:27 +0200

    test(veracrypt): colocate assay suite for systemPackages

---
+++ features/security/veracrypt/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { veracrypt = "veracrypt"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "veracrypt" {
+    systemPackages = assay.eq packages ''[ "veracrypt" ]'';
+  }
