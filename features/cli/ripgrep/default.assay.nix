afe64f315ba2db5b0d50e2ad27e3c288c1f36e8c
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:21 +0200

    test(ripgrep): colocate assay suite for systemPackages

---
+++ features/cli/ripgrep/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { ripgrep = "ripgrep"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "ripgrep" {
+    systemPackages = assay.eq packages ''[ "ripgrep" ]'';
+  }
