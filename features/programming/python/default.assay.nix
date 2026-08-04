54ee44d038a2e7147753d55c60463faa577404a3
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:27 +0200

    test(python): colocate assay suite for systemPackages

---
+++ features/programming/python/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { python312 = "python312"; uv = "uv"; pipx = "pipx"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "python" {
+    systemPackages = assay.eq packages ''[ "python312" "uv" "pipx" ]'';
+  }
