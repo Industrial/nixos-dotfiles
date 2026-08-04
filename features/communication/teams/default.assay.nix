6eb9a8f2ffebed1f1f92d88b888f20aca9281d62
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:22 +0200

    test(teams): colocate assay suite for systemPackages

---
+++ features/communication/teams/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { "teams-for-linux" = "teams-for-linux"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "teams" {
+    systemPackages = assay.eq packages ''[ "teams-for-linux" ]'';
+  }
