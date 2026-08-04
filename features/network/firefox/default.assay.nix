9e1cc4227a2febb4e5f8ef1c46506205891d02b8
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:25 +0200

    test(firefox): colocate assay suite for systemPackages

---
+++ features/network/firefox/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { librewolf = "librewolf"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "firefox" {
+    systemPackages = assay.eq packages ''[ "librewolf" ]'';
+  }
