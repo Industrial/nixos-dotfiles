803583b374c44298656e9240691cff8a1cf19eb7
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:36:07 +0200

    test(du): colocate assay suite

---
+++ features/cli/du/default.assay.nix
@@ -0,0 +1,22 @@
+# Colocated suite: wrapper derivation name is installed.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+(    let
+      pkgs = {
+        stdenv = { mkDerivation = args: args.name; };
+        eza = "eza";
+        settings = {};
+      };
+      settings = { useremail = "a@b.c"; hostname = "h"; username = "u"; };
+      mod = import ${modFile} { inherit pkgs; } // { };
+      # some wrappers also take settings
+      mod2 = builtins.tryEval (import ${modFile} { inherit pkgs settings; });
+      mod' = if mod2.success then mod2.value else (import ${modFile} { inherit pkgs; });
+    in mod'.environment.systemPackages)
+'';
+in
+  assay.suite "du" {
+    systemPackages = assay.eq packages ''[ "du" ]'';
+  }
