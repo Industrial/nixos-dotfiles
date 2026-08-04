4f2b71e1c0ce87baf0c4db956e93413f829867c3
Tom Wieland <tom.wieland@gmail.com>
2026-08-04 02:27:23 +0200

    test(awakened-poe-trade): colocate assay suite for systemPackages

---
+++ features/games/awakened-poe-trade/default.assay.nix
@@ -0,0 +1,14 @@
+# Colocated suite: systemPackages from stubbed pkgs.
+let
+  assay = import ./../../../common/assay/default.nix;
+  modFile = toString ./default.nix;
+  packages = ''
+    let
+      pkgs = { "awakened-poe-trade" = "awakened-poe-trade"; };
+      mod = import ${modFile} { inherit pkgs; };
+    in mod.environment.systemPackages
+  '';
+in
+  assay.suite "awakened-poe-trade" {
+    systemPackages = assay.eq packages ''[ "awakened-poe-trade" ]'';
+  }
