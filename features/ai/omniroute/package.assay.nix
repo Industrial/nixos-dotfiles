9192ff9d0d00e3dd348b20b295017a52048d1861
Tom Wieland <tom.wieland@gmail.com>
2026-08-15 01:50:13 +0200

    chore: remove omniroute from repo

--- features/ai/omniroute/package.assay.nix
+++
@@ -1,8 +0,0 @@
-# Colocated suite for features/ai/omniroute/package.nix
-let
-  assay = import ./../../../common/assay/default.nix;
-  src = builtins.readFile ./package.nix;
-in
-  assay.suite "package" {
-    isPackageExpr = assay.eq ((builtins.stringLength src) > 20) true;
-  }
